08b0cf7dd855f4d0aeb4a057564a32d03a7760a7
Azka Rohbiya Ramadani <azkarohbiya@gmail.com>
2022-03-16 22:49:26 +0700

    adding df2sqltext function

--- yelp_main.py
+++ yelp_main.py
@@ -24,11 +24,38 @@ def json2csv(df, dir_name):
         col = column+'.*' if 'struct' in dtype else column
         fined_columns.append(col)
     df.coalesce(1).select(fined_columns).write.option('header',True).csv(dir_name)
+
+def df2sqltext(df, name, mode='a', filename='yelp_createtable.sql'):
+    '''
+    Genereating dataframe to SQL language based on its columns and the datatypes
+
+    Params
+    ------
+        df       : spark frame
+        name     : table name for SQL language
+    '''
+	fined_columns = []
+	for column, dtype in df.dtypes:
+        col = column+'.*' if 'struct' in dtype else column
+        fined_columns.append(col)
+	col_dtype = df.select(fined_columns).dtypes
+	# create SQL text
+	with open(filename,mode) as f:
+        f.write('create table df_{}(\n'.format(name))
+        tmp = []
+        for col, dtype in col_dtype:
+            col = col.lower()
+            dtype = dtype.replace('string','varchar').replace('double','float8')
+            tmp.append('\t{} {}'.format(col, dtype))
+        f.write(',\n'.join(tmp))
+        f.write('\n);\n\n')
     
 if __name__ == '__main__':
     # testing
     df_spark('datasets/yelp_academic_dataset_business.json')
     # json2csv(df_spark.df, 'flatfile/business')
+    os.system('touch yelp_createtable.sql')
+    df2sqltext(df_spark.df, 'business')
     
     query='''select
                 business_id,    
